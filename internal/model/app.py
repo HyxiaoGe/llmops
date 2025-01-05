c429a3769dbf00b49ccb98ea3fc955c2534929aa
HyxiaoGe <18889592303@163.com>
2025-01-05 20:46:56 +0800

    feat: upgrade app model

--- internal/model/app.py
+++ internal/model/app.py
@@ -1,6 +1,3 @@
-import uuid
-from datetime import datetime
-
 from sqlalchemy import (
     Column,
     UUID,
@@ -8,7 +5,7 @@ from sqlalchemy import (
     Text,
     DateTime,
     PrimaryKeyConstraint,
-    Index,
+    Index, text,
 )
 
 from internal.extension.database_extension import db
@@ -22,11 +19,12 @@ class App(db.Model):
         Index("idx_app_account_id", "account_id"),
     )
 
-    id = Column(UUID, default=uuid.uuid4, nullable=False)
-    account_id = Column(UUID, nullable=False)
-    name = Column(String(255), default="", nullable=False)
-    icon = Column(String(255), default="", nullable=False)
-    description = Column(Text, default="", nullable=False)
-    status = Column(String(255), default="", nullable=False)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
-    created_at = Column(DateTime, default=datetime.now, nullable=False)
+    id = Column(UUID, nullable=False, server_default=text("uuid_generate_v4()"))
+    account_id = Column(UUID)
+    name = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    icon = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    description = Column(Text, nullable=False, server_default=text("''::text"))
+    status = Column(String(255), nullable=False, server_default=text("''::character varying"))
+    updated_at = Column(DateTime, nullable=False,
+                        server_default=text("CURRENT_TIMESTAMP(0)"), server_onupdate=text("CURRENT_TIMESTAMP(0)"))
+    created_at = Column(DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP(0)"))
